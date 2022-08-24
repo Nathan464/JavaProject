3d25a546330429adb732035610b15ac8efdba46c
Nathan464 <67219520+Nathan464@users.noreply.github.com>
2022-08-24 16:34:46 +0800

    UserService

--- src/main/java/com/nathan/service/user/UserService.java
+++ src/main/java/com/nathan/service/user/UserService.java
@@ -1,4 +1,27 @@
 package com.nathan.service.user;
 
+import com.nathan.pojo.User;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface UserService {
+    User login(String userCode, String passWord) throws SQLException;
+
+    boolean modifyPassword(int id, String newPassword) throws SQLException;
+
+    List<User> getUserList(String userName, int userRole, int currentPageNo,
+                           int pageSize) throws SQLException;
+
+    int getUserCount(String userName, int userRole) throws SQLException;
+
+    boolean addUser(User user) throws SQLException;
+
+    boolean isUserExist(String userCode) throws SQLException;
+
+    boolean deleteUser(int id) throws SQLException;
+
+    User viewUser(int id) throws SQLException;
+
+    boolean modifyUser(User user) throws SQLException;
 }
--- src/main/java/com/nathan/service/user/UserServiceImp.java
+++ src/main/java/com/nathan/service/user/UserServiceImp.java
@@ -1,4 +1,159 @@
 package com.nathan.service.user;
 
-public class UserServiceImp {
+import com.nathan.dao.BaseDao;
+import com.nathan.dao.user.UserDao;
+import com.nathan.dao.user.UserDaoImp;
+import com.nathan.pojo.User;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+public class UserServiceImp implements UserService{
+    private final UserDao userDao;
+    public UserServiceImp() {
+        userDao = new UserDaoImp();
+    }
+
+    @Override
+    public User login(String userCode, String passWord) throws SQLException {
+        return userDao.getLoginUser(userCode,passWord);
+    }
+
+    @Override
+    public boolean modifyPassword(int id, String newPassword) throws SQLException {
+        return userDao.modifyPassword(id,newPassword);
+    }
+
+    @Override
+    public List<User> getUserList(String userName, int userRole, int currentPageNo,
+                                  int pageSize) throws SQLException {
+        List<User> userList = null;
+        Connection connection = BaseDao.getConnection();
+        if (connection!=null){
+            try {
+                userList = userDao.getUserList(connection,userName,userRole,currentPageNo,pageSize);
+            }catch (SQLException e){
+                e.printStackTrace();
+            }finally {
+                BaseDao.closeResources(connection,null,null);
+            }
+        }
+        return userList;
+    }
+
+    @Override
+    public int getUserCount(String userName, int userRole) throws SQLException {
+        int count = 0;
+        Connection connection = BaseDao.getConnection();
+        if (connection!=null){
+            try {
+                count = userDao.getUserCount(connection,userName,userRole);
+            }catch (SQLException e){
+                e.printStackTrace();
+            }finally {
+                BaseDao.closeResources(connection,null,null);
+            }
+        }
+        return count;
+    }
+
+    @Override
+    public boolean addUser(User user) throws SQLException {
+        boolean flag = false;
+        Connection connection = BaseDao.getConnection();
+        if (connection!=null){
+            try {
+                connection.setAutoCommit(false);
+                flag = userDao.addUser(connection,user);
+                connection.commit();
+            }catch (SQLException exception){
+                try {
+                    connection.rollback();
+                }catch (SQLException e){
+                    e.printStackTrace();
+                }
+                exception.printStackTrace();
+            }finally {
+                BaseDao.closeResources(connection,null,null);
+            }
+        }
+        return flag;
+    }
+
+    @Override
+    public boolean isUserExist(String userCode) throws SQLException {
+        return userDao.isUserExist(userCode);
+    }
+
+    @Override
+    public boolean deleteUser(int id) throws SQLException {
+        boolean flag = true;
+        Connection connection = BaseDao.getConnection();
+        if (connection!=null){
+            try {
+                connection.setAutoCommit(false);
+                flag = userDao.deleterUser(connection,id);
+                connection.commit();
+            }catch (SQLException exception){
+                try {
+                    connection.rollback();
+                }catch (SQLException e){
+                    e.printStackTrace();
+                }
+                flag = false;
+                exception.printStackTrace();
+            }finally {
+                BaseDao.closeResources(connection,null,null);
+            }
+        }
+        return flag;
+    }
+
+    @Override
+    public User viewUser(int id) throws SQLException {
+        User user = null;
+        Connection connection = BaseDao.getConnection();
+        if (connection!=null){
+            try {
+                connection.setAutoCommit(false);
+                user = userDao.viewUser(connection,id);
+                connection.commit();
+            }catch (SQLException e){
+                try {
+                    connection.rollback();
+                }catch (SQLException exception){
+                    exception.printStackTrace();
+                }
+                e.printStackTrace();
+            }finally {
+                BaseDao.closeResources(connection,null,null);
+            }
+        }
+        return user;
+    }
+
+    @Override
+    public boolean modifyUser(User user) throws SQLException {
+        boolean flag = false;
+        Connection connection = BaseDao.getConnection();
+        if (connection!=null){
+            try{
+                connection.setAutoCommit(false);
+                flag = userDao.modifyUser(connection,user);
+                connection.commit();
+            }catch (SQLException exception){
+                flag = false;
+                try {
+                    connection.rollback();
+                }catch (SQLException e){
+                    e.printStackTrace();
+                }
+                exception.printStackTrace();
+            }finally {
+                BaseDao.closeResources(connection,null,null);
+            }
+        }
+        return flag;
+    }
 }
