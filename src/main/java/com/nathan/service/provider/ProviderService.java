fba1d5bdba2af25a14ce980a10221c8694ca29e0
Nathan464 <67219520+Nathan464@users.noreply.github.com>
2022-08-24 15:10:46 +0800

    ProviderService

--- src/main/java/com/nathan/service/provider/ProviderService.java
+++ src/main/java/com/nathan/service/provider/ProviderService.java
@@ -1,5 +1,19 @@
 package com.nathan.service.provider;
 
-public interface ProviderService {
+import com.nathan.pojo.Provider;
 
+import java.util.List;
+
+public interface ProviderService {
+    boolean add(Provider provider);
+
+    List<Provider> getProviderList(String proName, String proCode, Integer currentPageNo, Integer pageSize);
+
+    int getProviderCount(String proName, String proCode);
+
+    int deleteProviderById(String providerId);
+
+    Provider getProviderById(String providerId);
+
+    boolean modify(Provider provider);
 }
--- src/main/java/com/nathan/service/provider/ProviderServiceImp.java
+++ src/main/java/com/nathan/service/provider/ProviderServiceImp.java
@@ -1,4 +1,48 @@
 package com.nathan.service.provider;
 
-public class ProviderServiceImp {
+import com.nathan.dao.bill.BillDao;
+import com.nathan.dao.bill.BillDaoImp;
+import com.nathan.dao.provider.ProviderDao;
+import com.nathan.dao.provider.ProviderDaoImp;
+import com.nathan.pojo.Provider;
+
+import java.util.List;
+
+public class ProviderServiceImp implements ProviderService{
+    private final ProviderDao providerDao;
+    private final BillDao billDao;
+    public ProviderServiceImp(){
+        providerDao = new ProviderDaoImp();
+        billDao = new BillDaoImp();
+    }
+    @Override
+    public boolean add(Provider provider) {
+
+        return false;
+    }
+
+    @Override
+    public List<Provider> getProviderList(String proName, String proCode, Integer currentPageNo, Integer pageSize) {
+        return null;
+    }
+
+    @Override
+    public int getProviderCount(String proName, String proCode) {
+        return 0;
+    }
+
+    @Override
+    public int deleteProviderById(String providerId) {
+        return 0;
+    }
+
+    @Override
+    public Provider getProviderById(String providerId) {
+        return null;
+    }
+
+    @Override
+    public boolean modify(Provider provider) {
+        return false;
+    }
 }
