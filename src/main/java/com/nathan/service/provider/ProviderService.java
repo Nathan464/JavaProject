708fee2ea1d4b11ddbeac28dc8f79fbd90a224ee
Nathan464 <67219520+Nathan464@users.noreply.github.com>
2022-08-24 15:36:47 +0800

    ProviderService

--- src/main/java/com/nathan/service/provider/ProviderService.java
+++ src/main/java/com/nathan/service/provider/ProviderService.java
@@ -2,18 +2,20 @@ package com.nathan.service.provider;
 
 import com.nathan.pojo.Provider;
 
+import java.sql.SQLException;
 import java.util.List;
 
 public interface ProviderService {
-    boolean add(Provider provider);
+    boolean add(Provider provider) throws SQLException;
 
-    List<Provider> getProviderList(String proName, String proCode, Integer currentPageNo, Integer pageSize);
+    List<Provider> getProviderList(String proName, String proCode, Integer currentPageNo,
+                                   Integer pageSize) throws SQLException;
 
-    int getProviderCount(String proName, String proCode);
+    int getProviderCount(String proName, String proCode) throws SQLException;
 
-    int deleteProviderById(String providerId);
+    int deleteProviderById(String providerId) throws SQLException;
 
-    Provider getProviderById(String providerId);
+    Provider getProviderById(String providerId) throws SQLException;
 
-    boolean modify(Provider provider);
+    boolean modify(Provider provider) throws SQLException;
 }
--- src/main/java/com/nathan/service/provider/ProviderServiceImp.java
+++ src/main/java/com/nathan/service/provider/ProviderServiceImp.java
@@ -1,48 +1,137 @@
 package com.nathan.service.provider;
 
+import com.nathan.dao.BaseDao;
 import com.nathan.dao.bill.BillDao;
 import com.nathan.dao.bill.BillDaoImp;
 import com.nathan.dao.provider.ProviderDao;
 import com.nathan.dao.provider.ProviderDaoImp;
 import com.nathan.pojo.Provider;
 
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
-public class ProviderServiceImp implements ProviderService{
+public class ProviderServiceImp implements ProviderService {
     private final ProviderDao providerDao;
     private final BillDao billDao;
-    public ProviderServiceImp(){
+
+    public ProviderServiceImp() {
         providerDao = new ProviderDaoImp();
         billDao = new BillDaoImp();
     }
-    @Override
-    public boolean add(Provider provider) {
 
-        return false;
+    @Override
+    public boolean add(Provider provider) throws SQLException {
+        boolean flag = false;
+        Connection connection = null;
+        try {
+            connection = BaseDao.getConnection();
+            connection.setAutoCommit(false);
+            if (providerDao.add(connection, provider) > 0) {
+                flag = true;
+            }
+            connection.commit();
+        } catch (SQLException e) {
+            e.printStackTrace();
+            try {
+                connection.rollback();
+            } catch (SQLException exception) {
+                exception.printStackTrace();
+            }
+        } finally {
+            BaseDao.closeResources(connection, null, null);
+        }
+        return flag;
     }
 
     @Override
-    public List<Provider> getProviderList(String proName, String proCode, Integer currentPageNo, Integer pageSize) {
-        return null;
+    public List<Provider> getProviderList(String proName, String proCode, Integer currentPageNo,
+                                          Integer pageSize) throws SQLException {
+        List<Provider> providerList = new ArrayList<>();
+        Connection connection = null;
+        try {
+            connection = BaseDao.getConnection();
+            providerList = providerDao.getProviderList(connection, proName, proCode, currentPageNo, pageSize);
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        } finally {
+            BaseDao.closeResources(connection, null, null);
+        }
+        return providerList;
     }
 
     @Override
-    public int getProviderCount(String proName, String proCode) {
-        return 0;
+    public int getProviderCount(String proName, String proCode) throws SQLException {
+        int count = 0;
+        Connection connection = BaseDao.getConnection();
+        if (connection != null) {
+            try {
+                count = providerDao.getProviderCount(connection, proName, proCode);
+            } catch (SQLException exception) {
+                exception.printStackTrace();
+            } finally {
+                BaseDao.closeResources(connection, null, null);
+            }
+        }
+        return count;
     }
 
     @Override
-    public int deleteProviderById(String providerId) {
-        return 0;
+    public int deleteProviderById(String providerId) throws SQLException {
+        Connection connection = null;
+        int billCount = -1;
+        try {
+            connection = BaseDao.getConnection();
+            connection.setAutoCommit(false);
+            billCount = billDao.getBillCountByProviderId(connection, providerId);
+            if (billCount == 0) {
+                providerDao.deleteProviderById(connection, providerId);
+            }
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+            billCount = -1;
+            try {
+                connection.rollback();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        } finally {
+            BaseDao.closeResources(connection, null, null);
+        }
+        return billCount;
     }
 
     @Override
-    public Provider getProviderById(String providerId) {
-        return null;
+    public Provider getProviderById(String providerId) throws SQLException {
+        Provider provider;
+        Connection connection = null;
+        try {
+            connection = BaseDao.getConnection();
+            provider = providerDao.getProviderById(connection, providerId);
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+            provider = null;
+        } finally {
+            BaseDao.closeResources(connection, null, null);
+        }
+        return provider;
     }
 
     @Override
-    public boolean modify(Provider provider) {
-        return false;
+    public boolean modify(Provider provider) throws SQLException {
+        boolean flag = false;
+        Connection connection = null;
+        try {
+            connection = BaseDao.getConnection();
+            if (providerDao.modify(connection, provider) > 0) {
+                flag = true;
+            }
+        } catch (SQLException exception) {
+            exception.printStackTrace();
+        } finally {
+            BaseDao.closeResources(connection, null, null);
+        }
+        return flag;
     }
 }
