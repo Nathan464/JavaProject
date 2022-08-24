77237e106ddee0586b7f2d2c20a54fb54e4aa2a3
Nathan464 <67219520+Nathan464@users.noreply.github.com>
2022-08-24 16:46:15 +0800

    UserRoleService

--- src/main/java/com/nathan/service/userRole/UserRoleService.java
+++ src/main/java/com/nathan/service/userRole/UserRoleService.java
@@ -1,4 +1,10 @@
 package com.nathan.service.userRole;
 
+import com.nathan.pojo.Role;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public interface UserRoleService {
+    List<Role> getRoleList() throws SQLException;
 }
--- src/main/java/com/nathan/service/userRole/UserRoleServiceImp.java
+++ src/main/java/com/nathan/service/userRole/UserRoleServiceImp.java
@@ -1,4 +1,35 @@
 package com.nathan.service.userRole;
 
-public class UserRoleServiceImp {
+import com.nathan.dao.BaseDao;
+import com.nathan.dao.userRole.UserRoleDao;
+import com.nathan.dao.userRole.UserRoleDaoImp;
+import com.nathan.pojo.Role;
+import org.springframework.test.context.jdbc.Sql;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+
+public class UserRoleServiceImp implements UserRoleService {
+    private final UserRoleDao userRoleDao;
+
+    public UserRoleServiceImp() {
+        this.userRoleDao = new UserRoleDaoImp();
+    }
+
+    @Override
+    public List<Role> getRoleList() throws SQLException {
+        List<Role> roleList = null;
+        Connection connection = BaseDao.getConnection();
+        if (connection != null) {
+            try {
+                roleList = userRoleDao.getRoleList(connection);
+            } catch (SQLException e) {
+                e.printStackTrace();
+            } finally {
+                BaseDao.closeResources(connection, null, null);
+            }
+        }
+        return roleList;
+    }
 }
